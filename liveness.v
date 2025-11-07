1b2dc0b5a44ad561b2e2940492dfc4af3868e907
Samir Genaim <genaim@gmail.com>
2025-11-07 23:07:37 +0100

    more progress in liveness

--- liveness.v
+++ liveness.v
@@ -82,19 +82,19 @@ Module Liveness (D: DIALECT).
   CoInductive live_out  (p : SmartContractD.t) : FunctionName.t -> BlockID.t -> VarSet.t -> Prop :=
 
   (* Return block *)
-  | block_w_ret (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (rs: list SimpleExprD.t):
+  | lo_block_w_ret (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (rs: list SimpleExprD.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     BlockD.is_return_block b = Some rs -> (* it is an exit block and rs is the list of returned expressions *)
     live_out p fname bid (list_to_set (extract_yul_vars rs))
 
   (* Terminated block *)
-  | block_w_ter (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (s: VarSet.t):
+  | lo_block_w_ter (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     BlockD.is_terminated_block b = true -> (* it is an terminate block *)
     live_out p fname bid VarSet.empty 
 
   (* A block with a with jump *)
-  | block_w_jump (fname : FunctionName.t) (bid next_bid:  BlockID.t) (b next_b: BlockD.t) (s s': VarSet.t):
+  | lo_block_w_jump (fname : FunctionName.t) (bid next_bid:  BlockID.t) (b next_b: BlockD.t) (s s': VarSet.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     BlockD.is_jump_block b = Some next_bid -> (* the block ends with a jump, and next_bid is the id of the next block *)
     live_in p fname next_bid s -> (* s is the set of live variables at the entry of p/fname/next_bid *)
@@ -103,7 +103,7 @@ Module Liveness (D: DIALECT).
     live_out p fname bid s'  (* s' is the set of live variable at the exit of p/fname/bid *)
 
   (* A block with a conditional jump *)
-  | block_w_cond (fname : FunctionName.t) (bid next_bid_if_true next_bid_if_false:  BlockID.t) (cond_var: nat) (b next_b_if_true next_b_if_false: BlockD.t) (s1 s1' s2 s2': VarSet.t):
+  | lo_block_w_cond_jump (fname : FunctionName.t) (bid next_bid_if_true next_bid_if_false:  BlockID.t) (cond_var: nat) (b next_b_if_true next_b_if_false: BlockD.t) (s1 s1' s2 s2': VarSet.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     BlockD.is_cond_jump_block b = Some (cond_var, next_bid_if_true, next_bid_if_false) -> (* the block ends with a conditional jump, and next_bid_if_true and next_bid_if_false are the identifiers of the next blocks *)
     live_in p fname next_bid_if_true s1 ->  (* s1 is the set of live at the entry of p/fname/next_bid_if_true *)
@@ -115,7 +115,7 @@ Module Liveness (D: DIALECT).
     live_out p fname bid (VarSet.union s1' s2') (* s1' \/ s2' is the set of live at the exit ofx p/fname/bid *)
   with
     live_in (p : SmartContractD.t) : FunctionName.t -> BlockID.t -> VarSet.t -> Prop :=
-  | block_any (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (s: VarSet.t):
+  | li_block_any (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (s: VarSet.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     live_out p fname bid s -> (* s is the set of live variables at the exit of p/fname/bid *)
     live_in p fname bid (prop_live_set_bkw b.(BlockD.instructions) (add_jump_var_if_applicable b s)).
@@ -146,24 +146,20 @@ Module Liveness (D: DIALECT).
      that if 'r' is sound of all blocks, then live_in/live_out holds
      for all blocks etc.
    *)
-  Definition snd_block_info (p : SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t)
-    : Prop :=
+  Definition snd_block_out_info (p : SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : Prop :=
     exists f_info b_in_info b_out_info,
       (r f) = Some f_info /\ (* The live-variable information of the function exists *)
         f_info b.(BlockD.bid) = Some (b_in_info,b_out_info) /\ (* The live-variable information of the block exists *)
         match b.(BlockD.exit_info) with
         | ExitInfoD.Terminated =>
-            b_out_info = VarSet.empty /\
-              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+            b_out_info = VarSet.empty 
         | ExitInfoD.ReturnBlock rs => 
-            b_out_info = list_to_set (extract_yul_vars rs) /\
-              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+            b_out_info = list_to_set (extract_yul_vars rs) 
         | ExitInfoD.Jump next_bid =>
             exists next_b next_b_in_info next_b_out_info,
             SmartContractD.get_block p f next_bid = Some next_b /\ 
               f_info next_bid = Some (next_b_in_info,next_b_out_info) /\
-              b_out_info = apply_phi (BlockD.phi_function next_b b.(BlockD.bid)) next_b_in_info /\
-              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+              b_out_info = apply_phi (BlockD.phi_function next_b b.(BlockD.bid)) next_b_in_info
         | ExitInfoD.ConditionalJump cond_var next_bid_if_true next_bid_if_false => 
             exists next_b_if_true next_b_ift_in_info next_b_ift_out_info next_b_if_false next_b_iff_in_info next_b_iff_out_info,
             SmartContractD.get_block p f next_bid_if_true = Some next_b_if_true /\ 
@@ -172,17 +168,194 @@ Module Liveness (D: DIALECT).
               f_info next_bid_if_false = Some (next_b_iff_in_info,next_b_iff_out_info) /\
               b_out_info = VarSet.union
                              (apply_phi (BlockD.phi_function next_b_if_true b.(BlockD.bid)) next_b_ift_in_info)
-                             (apply_phi (BlockD.phi_function next_b_if_false b.(BlockD.bid)) next_b_iff_in_info) /\
-              b_in_info = prop_live_set_bkw b.(BlockD.instructions) (VarSet.add cond_var b_out_info)
+                             (apply_phi (BlockD.phi_function next_b_if_false b.(BlockD.bid)) next_b_iff_in_info)
         end.
 
+  Definition snd_block_in_info (p : SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : Prop :=
+    exists f_info b_in_info b_out_info,
+      (r f) = Some f_info /\ (* The live-variable information of the function exists *)
+        f_info b.(BlockD.bid) = Some (b_in_info,b_out_info) /\ (* The live-variable information of the block exists *)
+        b_in_info = prop_live_set_bkw b.(BlockD.instructions) (add_jump_var_if_applicable b b_out_info).
+  
+  Definition snd_block_info (p : SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t)
+    : Prop :=
+    snd_block_in_info p r f b /\ snd_block_out_info p r f b.
+  
 
   Definition snd_all_blocks_info (p : SmartContractD.t) (r: sc_live_info_t) : Prop :=
     forall f bid b,
       SmartContractD.get_block p f bid = Some b -> (* if the block exists *)
       snd_block_info p r f b. (* it has sound information *)
 
+
+  Lemma bid_b:
+    forall p f bid b,
+      SmartContractD.get_block p f bid = Some b -> (BlockD.bid b) = bid.
+  Proof.
+    intros p f bid b H_exists.
+    unfold SmartContractD.get_block in H_exists.
+    destruct (SmartContractD.get_function p f) as [|func]; try discriminate.
+    unfold SmartContractD.FunctionD.get_block in H_exists.
+    destruct (find (fun b : SmartContractD.FunctionD.BlockD.t => BlockID.eqb (SmartContractD.FunctionD.BlockD.bid b) bid) (SmartContractD.FunctionD.blocks t)) as [|block] eqn:H_find; try discriminate.
+    apply find_some in H_find.
+    destruct H_find as [_ H_find].
+    unfold BlockID.eqb in H_find.
+    rewrite Nat.eqb_eq in H_find.
+    injection H_exists as H_t0_b.
+    rewrite H_t0_b in H_find.
+    apply H_find.
+  Qed.
+
   
+  Lemma exit_info_vs_is_cond_jump:
+    forall b cond_var target_if_true target_if_false,
+      BlockD.exit_info b =
+        BlockD.ExitInfoD.ConditionalJump cond_var target_if_true target_if_false ->
+      BlockD.is_cond_jump_block b = Some (cond_var, target_if_true, target_if_false).
+  Proof.
+    intros b cond_var target_if_true target_if_false H.
+    unfold BlockD.is_cond_jump_block.
+    rewrite H.
+    reflexivity.
+  Qed.
+
+  Lemma exit_info_vs_is_jump:
+    forall b target,
+      BlockD.exit_info b =
+        BlockD.ExitInfoD.Jump target ->
+      BlockD.is_jump_block b = Some target.
+  Proof.
+    intros b target H.
+    unfold BlockD.is_jump_block.
+    rewrite H.
+    reflexivity.
+  Qed.
+
+  Lemma exit_info_vs_is_return:
+    forall b rs,
+      BlockD.exit_info b =
+        BlockD.ExitInfoD.ReturnBlock rs ->
+      BlockD.is_return_block b = Some rs.
+  Proof.
+    intros b rs H.
+    unfold BlockD.is_return_block.
+    rewrite H.
+    reflexivity.
+  Qed.
+
+  Lemma exit_info_vs_is_terminated:
+    forall b,
+      BlockD.exit_info b =
+        BlockD.ExitInfoD.Terminated ->
+      BlockD.is_terminated_block b = true.
+  Proof.
+    intros b H.
+    unfold BlockD.is_terminated_block.
+    rewrite H.
+    reflexivity.
+  Qed.
+  
+
+  CoFixpoint build_live_in (p : SmartContractD.t) (f: FunctionName.t) (bid: BlockID.t) (b: BlockD.t)
+    (r: sc_live_info_t) (f_info: fun_live_info_t) (b_in_info b_out_info: VarSet.t)
+    (H_snd_info: snd_all_blocks_info p r) (H_b_exists: SmartContractD.get_block p f bid = Some b)
+    (H_f_info: (r f) = Some f_info) (H_b_info: f_info bid = Some (b_in_info,b_out_info)) : live_in p f bid b_in_info
+  with build_live_out (p : SmartContractD.t) (f: FunctionName.t) (bid: BlockID.t) (b: BlockD.t)
+         (r: sc_live_info_t) (f_info: fun_live_info_t) (b_in_info b_out_info: VarSet.t)
+                           (H_snd_info: snd_all_blocks_info p r) (H_b_exists: SmartContractD.get_block p f bid = Some b)
+                           (H_f_info: (r f) = Some f_info) (H_b_info: f_info bid = Some (b_in_info,b_out_info)) : live_out p f bid b_out_info.
+  Proof.
+    (* the case of live_in p f bid b_in_info *)
+    - unfold snd_all_blocks_info in H_snd_info.
+      pose proof (H_snd_info f bid b H_b_exists) as H_snd_b_info.
+      unfold snd_block_info in H_snd_b_info.
+      destruct H_snd_b_info as [H_snd_b_in_info H_snd_b_out_info].
+      unfold snd_block_in_info in H_snd_b_in_info.
+      destruct H_snd_b_in_info as [f_info' [b_in_info' [b_out_info' [H_r_f [H_f_info' H_b_in_info']]]]].
+      rewrite (bid_b p f bid b H_b_exists) in H_f_info'.
+      
+      assert (H_f_info_info': f_info = f_info').
+      {rewrite H_f_info in H_r_f. injection H_r_f as H_r_f. apply H_r_f. }.
+
+      assert (H_b_in_info_info': b_in_info = b_in_info').
+      {rewrite <- H_f_info_info' in H_f_info'. rewrite H_b_info in H_f_info'. injection H_f_info' as H_b_in_info_info' H_b_out_info_info'. rewrite H_b_in_info_info'. reflexivity.}.
+
+      assert (H_b_out_info_info': b_out_info = b_out_info').
+      {rewrite <- H_f_info_info' in H_f_info'. rewrite H_b_info in H_f_info'. injection H_f_info' as _ H_b_out_info_info'. rewrite H_b_out_info_info'. reflexivity.}.
+
+      rewrite H_b_in_info_info'.
+      rewrite H_b_in_info'.
+      apply (@li_block_any p f bid b b_out_info' H_b_exists).
+
+      apply (build_live_out p f bid b r f_info' b_in_info' b_out_info' H_snd_info H_b_exists H_r_f  H_f_info').
+      
+        
+    (* the case of live_out p f bid b_in_info *)
+    - pose proof (H_snd_info f bid b H_b_exists) as H_snd_b_info.
+      unfold snd_block_info in H_snd_b_info.
+      destruct H_snd_b_info as [H_snd_b_in_info H_snd_b_out_info].
+      unfold snd_block_out_info in H_snd_b_out_info.
+      destruct H_snd_b_out_info as [f_info' [b_in_info' [b_out_info' [H_r_f [H_f_info' H_b_out_info']]]]].
+      rewrite (bid_b p f bid b H_b_exists) in H_f_info'.
+  
+      assert (H_f_info_info': f_info = f_info').
+      {rewrite H_f_info in H_r_f. injection H_r_f as H_r_f. apply H_r_f.}.
+
+      rewrite (bid_b p f bid b H_b_exists) in H_b_out_info'.
+
+      assert (H_b_out_info_info': b_out_info = b_out_info').
+      {rewrite <- H_f_info_info' in H_f_info'. rewrite H_b_info in H_f_info'. injection H_f_info' as _ H_b_out_info_info'. rewrite H_b_out_info_info'. reflexivity.}.
+            
+      destruct b.(BlockD.exit_info) as [cond_var target_if_true target_if_false | target | return_values | ] eqn:E_exit_info.
+
+      (* conditional jump *)
+      + destruct H_b_out_info' as [next_b_if_true [ next_b_ift_in_info [next_b_ift_out_info [next_b_if_false [ next_b_iff_in_info [next_b_iff_out_info [H_b_next_ift_exists [H_b_next_iff_exists [H_f_ift_info' [H_f_iff_info' H_b_out_info']]]]]]]]]].
+
+      rewrite H_b_out_info_info'.
+      
+      rewrite H_b_out_info'.
+        
+      apply (@lo_block_w_cond_jump p f bid target_if_true target_if_false cond_var b next_b_if_true next_b_if_false next_b_ift_in_info (apply_phi (BlockD.phi_function next_b_if_true bid) next_b_ift_in_info) next_b_iff_in_info (apply_phi (BlockD.phi_function next_b_if_false bid) next_b_iff_in_info) H_b_exists (exit_info_vs_is_cond_jump b cond_var target_if_true target_if_false E_exit_info ) (build_live_in p f target_if_true next_b_if_true r f_info' next_b_ift_in_info next_b_ift_out_info H_snd_info H_b_next_ift_exists H_r_f H_f_ift_info') (build_live_in p f target_if_false next_b_if_false r f_info' next_b_iff_in_info next_b_iff_out_info H_snd_info H_b_next_iff_exists H_r_f H_f_iff_info') H_b_next_ift_exists H_b_next_iff_exists  eq_refl eq_refl).
+
+      (* jump *)
+      + destruct H_b_out_info' as [next_b [next_b_in_info [next_b_out_info [H_b_next_exists [H_f_next_b_info' H_next_b_out_info']]]]].
+
+      rewrite H_b_out_info_info'.
+      rewrite H_next_b_out_info'.
+
+      apply (@lo_block_w_jump p f bid target b next_b next_b_in_info (apply_phi (BlockD.phi_function next_b bid) next_b_in_info) H_b_exists (exit_info_vs_is_jump b target E_exit_info) (build_live_in p f target next_b r f_info' next_b_in_info next_b_out_info H_snd_info H_b_next_exists H_r_f H_f_next_b_info') H_b_next_exists eq_refl).      
+
+      (* return *)
+      + rewrite H_b_out_info_info'.
+        rewrite H_b_out_info'.
+        apply (@lo_block_w_ret p f bid b return_values H_b_exists (exit_info_vs_is_return b return_values E_exit_info)).
+
+      (* teminated *)
+      + rewrite H_b_out_info_info'.
+        rewrite H_b_out_info'.
+        apply (@lo_block_w_ter p f bid b H_b_exists (exit_info_vs_is_terminated b E_exit_info)).
+  Defined.
+
+  Lemma snd_all_blocks_info_to_bid_info:
+    forall (p : SmartContractD.t) (r: sc_live_info_t),
+      snd_all_blocks_info p r ->  
+      forall f bid b,
+        SmartContractD.get_block p f bid = Some b ->
+        exists f_info b_in_info b_out_info,
+          (r f) = Some f_info /\
+            f_info bid = Some (b_in_info,b_out_info).
+    Proof.
+      intros p r H_snd_info f bid b H_b_exists.
+      pose proof (H_snd_info f bid b H_b_exists) as H_b_info_snd.
+      unfold snd_block_info in  H_b_info_snd.
+      destruct H_b_info_snd as [H_b_in_info_snd _].
+      unfold snd_block_in_info in H_b_in_info_snd.
+      rewrite (bid_b p f bid b H_b_exists) in H_b_in_info_snd.
+      destruct H_b_in_info_snd as [f_info [b_in_info [b_out_info [H_r_f [H_f_info _]]]]].
+      exists f_info. exists b_in_info. exists b_out_info.
+      split; assumption.
+    Qed.
+
   Theorem snd_info:
     forall (p : SmartContractD.t) (r: sc_live_info_t),
       snd_all_blocks_info p r ->  
@@ -193,8 +366,21 @@ Module Liveness (D: DIALECT).
             f_info bid = Some (b_in_info,b_out_info) /\
             live_in p f bid b_in_info /\
             live_out p f bid b_out_info.
-  
-      
+  Proof.
+    intros p r H_snd_info f bid b H_b_exists.
+    pose proof (H_snd_info f bid b H_b_exists) as H_b_info_snd.
+    unfold snd_block_info in  H_b_info_snd.
+    destruct H_b_info_snd as [H_b_in_info_snd H_b_out_info_snd].
+    pose proof (snd_all_blocks_info_to_bid_info p r H_snd_info f bid b H_b_exists) as H_snd_all_blocks_info_to_bid_info.
+    destruct H_snd_all_blocks_info_to_bid_info as [f_info [b_in_info [b_out_info [H_r_f H_f_info]]]].
+    exists f_info. exists b_in_info. exists b_out_info.
+    repeat split.
+    - assumption.
+    - assumption.
+    - apply (build_live_in p f bid b r f_info b_in_info b_out_info H_snd_info H_b_exists H_r_f H_f_info).
+    - apply (build_live_out p f bid b r f_info b_in_info b_out_info H_snd_info H_b_exists H_r_f H_f_info).
+  Qed.
+    
 
 End Liveness.
 
