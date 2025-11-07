2dcc2ad37f0c178690e071a7141dfe68160e85dd
Samir Genaim <genaim@gmail.com>
2025-11-07 14:42:08 +0100

    some progress in the live variable analysis

--- liveness.v
+++ liveness.v
@@ -7,6 +7,8 @@ Require Import MSets.
 Require Import MSetList.
 Require Import Arith.
 
+Open Scope nat_scope.
+
 
 Module VarSet := Make Nat_as_OT.
 
@@ -17,24 +19,10 @@ Module Liveness (D: DIALECT).
   Module SmartContractD := SmallStepD.SmartContractD.
   Module FunctionD := SmartContractD.FunctionD.
   Module BlockD := SmartContractD.BlockD.
+  Module ExitInfoD := BlockD.ExitInfoD.
   Module InstructionD := BlockD.InstructionD.
   Module YULVariableMapD := BlockD.PhiInfoD.YULVariableMapD.
   Module SimpleExprD := BlockD.PhiInfoD.YULVariableMapD.SimpleExprD.
-  
-  Definition is_end_of_block (p : SmartContractD.t) (fname : FunctionName.t) (bid:  BlockID.t) (pc: nat) :=
-    match (SmartContractD.get_instruction p fname bid pc) with
-    | None => false
-    | Some _ => true
-    end.
-
-  (* define a relaion between a list an its corresponding set *)
-  Inductive list_eq_set : list nat -> VarSet.t -> Prop :=
-  | empty:
-    list_eq_set nil VarSet.empty
-  | rec (x: nat) (xs: list nat) (s1 s2: VarSet.t):
-    list_eq_set xs s1 ->
-    VarSet.add x s1 = s2 ->
-    list_eq_set (x::xs) s2.
 
   (* convert a list to a set *)
   Fixpoint list_to_set (l : list nat) : VarSet.t :=
@@ -42,24 +30,6 @@ Module Liveness (D: DIALECT).
     | nil => VarSet.empty
     | x::xs => VarSet.add x (list_to_set xs)
     end.
-
-  Lemma list_to_set_snd:
-    forall l s, list_to_set l = s -> list_eq_set l s.
-  Proof.
-    induction l as [| a l'].
-    - intros s.
-      intros H.
-      simpl in H.
-      rewrite <- H.
-      apply empty.
-    - intros s.
-      intros H.
-      simpl in H.
-      apply rec with (s1 := (list_to_set l')).
-      + apply (IHl' (list_to_set l')).
-        reflexivity.
-      + apply H.
-  Qed.
 
   Fixpoint extract_yul_vars (l : list SimpleExprD.t) : list nat :=
     match l with
@@ -87,59 +57,144 @@ Module Liveness (D: DIALECT).
         end
     end.
 
-
-  (** This is the defintion of liveness
-      =================================
-
-      It is the way the standard live property is defined in dataflow
-      analysis.
-
-      'live_varset p f id pc x' means that the set of variables 'x' is
-      live at program point 'p/f/id/pc'. I.e., these variables are
-      used in the rest of the execution at least once. 
-
-   **)
+  Fixpoint prop_live_set_bkw (l: list InstructionD.t) (s: VarSet.t) : VarSet.t  :=
+    match l with
+    | nil => s
+    | i::l' =>
+        let s' :=  prop_live_set_bkw l' s in
+        let inset := list_to_set (extract_yul_vars i.(InstructionD.input)) in
+        let outset := list_to_set i.(InstructionD.output) in
+        (VarSet.union (VarSet.diff s' outset) inset)
+    end.
   
-  Inductive live_varset  (p : SmartContractD.t) : FunctionName.t -> BlockID.t -> nat -> VarSet.t -> Prop :=
+  Definition add_jump_var_if_applicable (b: BlockD.t) (s: VarSet.t) :=
+    match b.(BlockD.exit_info) with
+    | ExitInfoD.ConditionalJump cond_var _ _ => VarSet.add cond_var s
+    | _ => s
+      end.
+  
+  (*
+    Th  e following co-inductive defintions is for live variable properties.
+    
+    live_out p f bid s: s is the set of live variables at the exit of the block p/f/bid
+    live_in p f bid s: s is the set of live variables at the entry of the block p/f/bid
+   *)
+  CoInductive live_out  (p : SmartContractD.t) : FunctionName.t -> BlockID.t -> VarSet.t -> Prop :=
 
-  (* At an exit block, only the return variables are alive *)
-  | end_of_block_exit_pp (fname : FunctionName.t) (bid :  BlockID.t) (pc : nat) (b: BlockD.t) (rs: list SimpleExprD.t) (s: VarSet.t):
+  (* Return block *)
+  | block_w_ret (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (rs: list SimpleExprD.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
-    is_end_of_block p fname bid pc = true -> (* p/fname/bid/pc corresponds to an end of a block *)
-    BlockD.is_exit_block b = Some rs -> (* rs is the list of returned expressions *)
-    list_to_set (extract_yul_vars rs) = s -> (* s is the set representing rs *)
-    live_varset p fname bid pc s (* s is alive at  p/fname/bid/pc *)
+    BlockD.is_return_block b = Some rs -> (* it is an exit block and rs is the list of returned expressions *)
+    live_out p fname bid (list_to_set (extract_yul_vars rs))
 
-  (* At the end of block with a with jump, the live variables are those a live at the entry of the next block + renaming wrt. the corresponding phi function *)
-  | end_of_block_jump_pp (fname : FunctionName.t) (bid next_bid:  BlockID.t) (pc : nat) (b next_b: BlockD.t) (s s': VarSet.t):
+  (* Terminated block *)
+  | block_w_ter (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (s: VarSet.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
-    is_end_of_block p fname bid pc = true -> (* p/fname/bid/pc corresponds to an end of a block *)
+    BlockD.is_terminated_block b = true -> (* it is an terminate block *)
+    live_out p fname bid VarSet.empty 
+
+  (* A block with a with jump *)
+  | block_w_jump (fname : FunctionName.t) (bid next_bid:  BlockID.t) (b next_b: BlockD.t) (s s': VarSet.t):
+    SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
     BlockD.is_jump_block b = Some next_bid -> (* the block ends with a jump, and next_bid is the id of the next block *)
-    live_varset p fname next_bid 0 s -> (* s is the set of live variables at p/fname/next_bid/0 *)
+    live_in p fname next_bid s -> (* s is the set of live variables at the entry of p/fname/next_bid *)
     SmartContractD.get_block p fname next_bid = Some next_b -> (* next_b is the block with id next_bid *)
     apply_phi (BlockD.phi_function next_b bid) s = s' -> (* apply -- in a reverse way -- the corresponding phi function on s to obain s' *)
-    live_varset p fname bid pc s'  (* s' is alive at  p/fname/bid/pc *)
+    live_out p fname bid s'  (* s' is the set of live variable at the exit of p/fname/bid *)
 
-  (* At the end of block with a conditional jump, the live variables are those live at the entry of wither of the next next blocks -- after renaming wrt. the corresponding phi function -- and the conditional variable *)
-  | end_of_block_cond_jump_pp (fname : FunctionName.t) (bid next_bid_if_true next_bid_if_false:  BlockID.t) (pc cond_var: nat) (b next_b_if_true next_b_if_false: BlockD.t) (s1 s1' s2 s2': VarSet.t):
+  (* A block with a conditional jump *)
+  | block_w_cond (fname : FunctionName.t) (bid next_bid_if_true next_bid_if_false:  BlockID.t) (cond_var: nat) (b next_b_if_true next_b_if_false: BlockD.t) (s1 s1' s2 s2': VarSet.t):
     SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
-    is_end_of_block p fname bid pc = true ->  (* p/fname/bid/pc corresponds to an end of a block *)
-    BlockD.is_cond_jump_block b = Some (cond_var, next_bid_if_true, next_bid_if_false) -> (* the block ends with a jump, and next_bid_if_true and next_bid_if_false are the identifiers of the next blocks *)
-    live_varset p fname next_bid_if_true 0 s1 ->  (* s1 is the set of live variables at p/fname/next_bid_if_true/0 *)
-    live_varset p fname next_bid_if_false 0 s2 -> (* s2 is the set of live variables at p/fname/next_bid_if_false/0 *)
+    BlockD.is_cond_jump_block b = Some (cond_var, next_bid_if_true, next_bid_if_false) -> (* the block ends with a conditional jump, and next_bid_if_true and next_bid_if_false are the identifiers of the next blocks *)
+    live_in p fname next_bid_if_true s1 ->  (* s1 is the set of live at the entry of p/fname/next_bid_if_true *)
+    live_in p fname next_bid_if_false s2 -> (* s2 is the set of live variables at the entry of p/fname/next_bid_if_false *)
     SmartContractD.get_block p fname next_bid_if_true = Some next_b_if_true -> (* next_b_if_true is the block with id next_bid_if_true *)
     SmartContractD.get_block p fname next_bid_if_false = Some next_b_if_false -> (* next_b_if_false is the block with id next_bid_if_false *)
     apply_phi (BlockD.phi_function next_b_if_true bid) s1 = s1' -> (* apply -- in a reverse way -- the corresponding phi function on s1 to obain s1' *)
     apply_phi (BlockD.phi_function next_b_if_false bid) s2 = s2' -> (* apply -- in a reverse way -- the corresponding phi function on s2 to obain s2' *)
-    live_varset p fname bid pc (VarSet.add cond_var (VarSet.union s1' s2')) (* s1' \/ s2' is alive at  p/fname/bid/pc *)
+    live_out p fname bid (VarSet.union s1' s2') (* s1' \/ s2' is the set of live at the exit ofx p/fname/bid *)
+  with
+    live_in (p : SmartContractD.t) : FunctionName.t -> BlockID.t -> VarSet.t -> Prop :=
+  | block_any (fname : FunctionName.t) (bid :  BlockID.t) (b: BlockD.t) (s: VarSet.t):
+    SmartContractD.get_block p fname bid = Some b -> (* the block exists *)
+    live_out p fname bid s -> (* s is the set of live variables at the exit of p/fname/bid *)
+    live_in p fname bid (prop_live_set_bkw b.(BlockD.instructions) (add_jump_var_if_applicable b s)).
 
-  (* Execution of an instruction (opcode, function call, or a simple assignment) *)
-  | instr_pp (fname : FunctionName.t) (bid :  BlockID.t) (pc : nat) (b: BlockD.t) (instr : InstructionD.t) (s outvars invars: VarSet.t):
-    live_varset p fname bid (pc+1)%nat s -> (* s is the set of live vars at pc+1 *)
-    SmartContractD.get_instruction p fname bid pc = Some instr -> (* 'instr' is the current instructions at p/fname/bid/pc *)
-    list_eq_set (extract_yul_vars instr.(SmartContractD.BlockD.InstructionD.input)) invars -> (* invars is the set of input variables *)
-    list_eq_set instr.(SmartContractD.BlockD.InstructionD.output) outvars -> (* outvars is the set of output variables *)
-    live_varset p fname bid pc (VarSet.union (VarSet.diff s outvars) invars). (* (s\outvars) \/ invars is alive at  p/fname/bid/pc *)
+  (* The following types are used to define the result of a live-variable analysis *)
+
+  Definition block_live_info_t := nat -> option VarSet.t.
+  Definition fun_live_info_t := BlockID.t -> option (VarSet.t * VarSet.t).
+  Definition sc_live_info_t := FunctionName.t -> option fun_live_info_t.
+
+
+  (* All block have live-variable results in r *)
+  Definition snd_res_for_all_blocks (p : SmartContractD.t)  (r: sc_live_info_t) : Prop :=
+    forall bid f b,
+      SmartContractD.get_block p f bid = Some b -> 
+      exists bid_res fr,
+        r f = Some fr /\ fr bid = bid_res.
+
+
+  (*
+     This defines a proposition stating that the live-variable
+     information for p/f/bid in 'r' exists, and it is sound.
+
+     Soundness here means that some relations between the in/out
+     information of each block holds. It is pretty much follows what
+     is defined in the definition of live_in/live_out/etc. But we do
+     not use those defintions yet, they will be used lated to prove
+     that if 'r' is sound of all blocks, then live_in/live_out holds
+     for all blocks etc.
+   *)
+  Definition snd_block_info (p : SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t)
+    : Prop :=
+    exists f_info b_in_info b_out_info,
+      (r f) = Some f_info /\ (* The live-variable information of the function exists *)
+        f_info b.(BlockD.bid) = Some (b_in_info,b_out_info) /\ (* The live-variable information of the block exists *)
+        match b.(BlockD.exit_info) with
+        | ExitInfoD.Terminated =>
+            b_out_info = VarSet.empty /\
+              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+        | ExitInfoD.ReturnBlock rs => 
+            b_out_info = list_to_set (extract_yul_vars rs) /\
+              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+        | ExitInfoD.Jump next_bid =>
+            exists next_b next_b_in_info next_b_out_info,
+            SmartContractD.get_block p f next_bid = Some next_b /\ 
+              f_info next_bid = Some (next_b_in_info,next_b_out_info) /\
+              b_out_info = apply_phi (BlockD.phi_function next_b b.(BlockD.bid)) next_b_in_info /\
+              b_in_info = prop_live_set_bkw b.(BlockD.instructions) b_out_info
+        | ExitInfoD.ConditionalJump cond_var next_bid_if_true next_bid_if_false => 
+            exists next_b_if_true next_b_ift_in_info next_b_ift_out_info next_b_if_false next_b_iff_in_info next_b_iff_out_info,
+            SmartContractD.get_block p f next_bid_if_true = Some next_b_if_true /\ 
+            SmartContractD.get_block p f next_bid_if_false = Some next_b_if_false /\ 
+              f_info next_bid_if_true = Some (next_b_ift_in_info,next_b_ift_out_info) /\
+              f_info next_bid_if_false = Some (next_b_iff_in_info,next_b_iff_out_info) /\
+              b_out_info = VarSet.union
+                             (apply_phi (BlockD.phi_function next_b_if_true b.(BlockD.bid)) next_b_ift_in_info)
+                             (apply_phi (BlockD.phi_function next_b_if_false b.(BlockD.bid)) next_b_iff_in_info) /\
+              b_in_info = prop_live_set_bkw b.(BlockD.instructions) (VarSet.add cond_var b_out_info)
+        end.
+
+
+  Definition snd_all_blocks_info (p : SmartContractD.t) (r: sc_live_info_t) : Prop :=
+    forall f bid b,
+      SmartContractD.get_block p f bid = Some b -> (* if the block exists *)
+      snd_block_info p r f b. (* it has sound information *)
+
+  
+  Theorem snd_info:
+    forall (p : SmartContractD.t) (r: sc_live_info_t),
+      snd_all_blocks_info p r ->  
+      forall f bid b,
+        SmartContractD.get_block p f bid = Some b ->
+        exists f_info b_in_info b_out_info,
+          (r f) = Some f_info /\
+            f_info bid = Some (b_in_info,b_out_info) /\
+            live_in p f bid b_in_info /\
+            live_out p f bid b_out_info.
+  
+      
 
 End Liveness.
 
