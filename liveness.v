22dfc7899bdcef038c82476c0144657c1479ddb7
Samir Genaim <genaim@gmail.com>
2025-11-24 07:52:41 +0100

    Added a checker that allows non-optimal results

--- liveness.v
+++ liveness.v
@@ -152,7 +152,8 @@ Module Liveness (D: DIALECT).
       end
     end.
   
- 
+
+  
   Definition check_live_out (p: SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : bool :=
     match (r f) with
     | None => false
@@ -212,6 +213,7 @@ Module Liveness (D: DIALECT).
                 else false
     end.
 
+
   Fixpoint check_functions (fs: list FunctionD.t) (p: SmartContractD.t) (r: sc_live_info_t) :=
     match fs with
     | nil => true
@@ -224,4 +226,130 @@ Module Liveness (D: DIALECT).
     check_functions p.(SmartContractD.functions) p r.
 
 
+  (* A version that uses subset instead of equal *)
+
+  Definition check_live_in_subset (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : bool :=
+    match (r f) with
+    | None => false
+    | Some f_info =>
+        match f_info b.(BlockD.bid) with
+        | None => false
+        | Some (b_in_info,b_out_info) =>
+            VarSet.subset (prop_live_set_bkw b.(BlockD.instructions) (add_jump_var_if_applicable b b_out_info)) b_in_info 
+        end
+    end.
+
+  Definition check_live_out_subset (p: SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : bool :=
+    match (r f) with
+    | None => false
+    | Some f_info =>
+        match (f_info b.(BlockD.bid)) with
+        | None => false
+        | Some (b_in_info,b_out_info) =>
+            match b.(BlockD.exit_info) with
+            | ExitInfoD.Terminated =>
+                VarSet.subset VarSet.empty b_out_info
+            | ExitInfoD.ReturnBlock rs => 
+                VarSet.subset (list_to_set (extract_yul_vars rs)) b_out_info
+            | ExitInfoD.Jump next_bid =>
+                match (SmartContractD.get_block p f next_bid) with
+                | None => false
+                | Some next_b =>
+                    match (f_info next_bid) with
+                    | None => false
+                    | Some (next_b_in_info,next_b_out_info) =>
+                        VarSet.subset (apply_inv_phi (BlockD.phi_function next_b b.(BlockD.bid)) next_b_in_info) b_out_info
+                    end
+                end
+            | ExitInfoD.ConditionalJump cond_var next_bid_if_true next_bid_if_false =>
+                match (SmartContractD.get_block p f next_bid_if_true) with
+                | None => false
+                | Some next_b_if_true =>
+                    match (SmartContractD.get_block p f next_bid_if_false) with
+                    | None => false
+                    | Some next_b_if_false =>
+                        match (f_info next_bid_if_true) with
+                        | None => false
+                        | Some (next_b_ift_in_info,next_b_ift_out_info) =>
+                            match (f_info next_bid_if_false) with
+                            | None => false
+                            | Some (next_b_iff_in_info,next_b_iff_out_info) =>
+                                VarSet.subset (VarSet.union
+                                                 (apply_inv_phi (BlockD.phi_function next_b_if_true b.(BlockD.bid)) next_b_ift_in_info)
+                                                 (apply_inv_phi (BlockD.phi_function next_b_if_false b.(BlockD.bid)) next_b_iff_in_info)) b_out_info 
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end.
+
+  
+  Definition check_live_subset (p: SmartContractD.t) (r: sc_live_info_t) (f: FunctionName.t) (b: BlockD.t) : bool :=
+    if (check_live_in_subset r f b) then check_live_out_subset p r f b else false.
+
+  Fixpoint check_blocks_subset (bs: list BlockD.t) (fname: FunctionName.t) (p: SmartContractD.t) (r: sc_live_info_t) :=
+    match bs with
+    | nil => true
+    | b::bs' => if (check_live_subset p r fname b)
+                then check_blocks_subset bs' fname p r
+                else false
+    end.
+
+
+  Fixpoint check_functions_subset (fs: list FunctionD.t) (p: SmartContractD.t) (r: sc_live_info_t) :=
+    match fs with
+    | nil => true
+    | f::fs' => if (check_blocks_subset f.(FunctionD.blocks) f.(FunctionD.name) p r)
+                then check_functions_subset fs' p r
+                else false
+    end.
+  
+  Definition check_smart_contract_subset (p: SmartContractD.t) (r: sc_live_info_t) :=
+    check_functions_subset p.(SmartContractD.functions) p r.
+
+  (* end of version with subset *)
+  
+  Fixpoint nodupb {A : Type} (eqb : A -> A -> bool) (l : list A) : bool :=
+    match l with
+    | nil => true
+    | h :: t => (negb (existsb (eqb h) t)) && (nodupb eqb t)
+    end.
+
+  Lemma nodup_NoDup {A: Type}:
+    forall (eqb: A -> A -> bool),
+      (forall a b, eqb a b = true -> a = b) ->
+      forall l,
+        nodupb eqb l = true -> NoDup l.
+  Proof.
+    intros eqb H_eqb_eq.
+    induction l as [|a l'].
+    - intros.
+      apply NoDup_nil_2.
+    - intros H_nodupb.
+      apply NoDup_cons_2.
+      + simpl in H_nodupb.
+        apply andb_prop in H_nodupb.
+        destruct H_nodupb as [H_nodupb_l H_nodupb_r].
+        rewrite negb_true_iff in H_nodupb_l.
+        pose proof (@existb_True A).
+        Admitted.
+  
+  Definition all_block_ids_are_different (f: FunctionD.t): bool :=
+    nodupb (fun b1 b2 => Nat.eqb b1.(BlockD.bid) b2.(BlockD.bid)) f.(FunctionD.blocks).
+
+  Definition check_valid_function (f: FunctionD.t): bool :=
+    all_block_ids_are_different f.
+  
+  Definition check_valid_functions (l: list FunctionD.t): bool :=
+    fold_right (fun b a => a && check_valid_function b) true l.
+
+  Definition check_function_names_are_different (l: list FunctionD.t): bool :=
+    nodupb (fun f1 f2 => String.eqb f1.(FunctionD.name) f2.(FunctionD.name)) l.
+
+  Definition smart_contrac (p: SmartContractD.t): bool :=
+    check_function_names_are_different p.(SmartContractD.functions) &&
+    check_valid_functions p.(SmartContractD.functions).
+    
 End Liveness.
